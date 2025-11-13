cfe6de2f52de1da077a5d398312ff64c61900d89
auladwd <auladidea@gmail.com>
2025-11-13 20:27:11 +0600

    Modify Notfound

--- src/pages/Error/NotFound.jsx
+++ src/pages/Error/NotFound.jsx
@@ -6,7 +6,7 @@ const NotFound = () => {
   return (
     <div className="flex flex-col justify-center items-center h-[70vh] text-center">
       <h1 className="text-5xl font-bold mb-3">404</h1>
-      <p className="mb-4 text-gray-600">
+      <p className="mb-4 text-gray-500">
         Oops! The page you are looking for does not exist.
       </p>
       <Link to="/" className="btn btn-primary">
